751b359ec81d6dcae125cc3a2fc26e84adca659c
Beck <kbeck@IT-AB.DE>
2016-11-16 08:20:03 +0100

    Suchfunktion (ufnertig)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
 use AppBundle\Entity\Firma;
@@ -14,6 +15,15 @@ use AppBundle\Form\AnsprechpartnerType;
 
 class DefaultController extends Controller
 {
+	/**
+	 * Firmensuchfunktion
+	 */
+	public function compsearchAction(Request $request, $value){
+		$repository = $this->getDoctrine()->getRepository('AppBundle:Firma');
+        $firma = $repository->findByName('$value');
+        echo $twig->render('index.html.twig', array('firma' => $firma));
+	}
+	
     /**
      * @Route("/", name="homepage")
      */
