d8a66615f3420f0f835711dd8717c97b9eb80a4c
Daniel Ciecior <daniel_ciecior@yahoo.de>
2016-11-28 13:55:09 +0100

    solved a delete bug when deleting user profiles

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -11,167 +11,177 @@ use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
 use AppBundle\Form\ProfilType;
 
-class UserController extends Controller
-{														/* User Funktionen */
-	
-	/** Neuen Benutzer mit Username, Email und Password anlegen
-	 * @Route("/create/user", name="registration")
-	 */
-	public function userAction(Request $request){
-		$user = new User();
-		$form = $this->createForm("AppBundle\Form\UserType", $user);
-		$form->handleRequest($request);
-        
+class UserController extends Controller { /* User Funktionen */
+
+    /** Neuen Benutzer mit Username, Email und Password anlegen
+     * @Route("/create/user", name="registration")
+     */
+    public function userAction(Request $request) {
+        $user = new User();
+        $form = $this->createForm("AppBundle\Form\UserType", $user);
+        $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-			$em = $this->getDoctrine()->getManager();
-			
-			$factory = $this->get('security.encoder_factory');
-			
-			$encoder = $factory->getEncoder($user);
-			$password = $encoder->encodePassword($user->getPassword(), $user->getSalt());
-			$user->setPassword($password);
-			
+            $em = $this->getDoctrine()->getManager();
+
+            $factory = $this->get('security.encoder_factory');
+
+            $encoder = $factory->getEncoder($user);
+            $password = $encoder->encodePassword($user->getPassword(), $user->getSalt());
+            $user->setPassword($password);
+
             $em->persist($user);
             $em->flush();
-            $msg = "Account für ".$user->getEmail()." wurde erfolgreich registriert!";
+            $msg = "Account für " . $user->getEmail() . " wurde erfolgreich registriert!";
             return $this->render('default/confirm.html.twig', array(
-                'message' => $msg
+                        'message' => $msg
             ));
         }
-		
-		return $this->render('default/form/registration.html.twig', array(
-				'form' => $form->createView()
-		));
-	}
-	
-	/** Wenn eingeloggt, Seine eigenen Benutzerdaten mit Editierfunktion anzeigen
+
+        return $this->render('default/form/registration.html.twig', array(
+                    'form' => $form->createView()
+        ));
+    }
+
+    /** Wenn eingeloggt, Seine eigenen Benutzerdaten mit Editierfunktion anzeigen
      * @Route("/edit/user", name="edituser")
      * @Security("has_role('ROLE_USER')")
      */
-    public function editUserAction(Request $request)
-    {
+    public function editUserAction(Request $request) {
         $uid = $this->get('security.token_storage')->getToken()->getUser()->getId();
         $user = $this->getDoctrine()
-                        ->getRepository('AppBundle:User')
-                        ->find($uid);
+                ->getRepository('AppBundle:User')
+                ->find($uid);
         $form = $this->createForm("AppBundle\Form\ProfilType", $user);
-		$form->handleRequest($request);
-        
+        $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            if ($request->request->has('delete'))
-            {
+            if ($request->request->has('delete')) {
                 return $this->redirectToRoute('deleteuser');
             }
-			$em = $this->getDoctrine()->getManager();
-			
+            $em = $this->getDoctrine()->getManager();
+
             $em->persist($user);
             $em->flush();
         }
-        
+
         return $this->render('edit/user.html.twig', array(
-				'user' => $user,
-				'form' => $form->createView()
+                    'user' => $user,
+                    'form' => $form->createView()
         ));
     }
-	
-	/** Zeigt eine Liste aller registrierten Benutzer für die Mitarbeiter
-    * @Route("/show/users", name="showusers")
-    * @Security("has_role('ROLE_STAFF')")
-    */
-    public function showUsersAction(Request $request)
-    {
+
+    /** Zeigt eine Liste aller registrierten Benutzer für die Mitarbeiter
+     * @Route("/show/users", name="showusers")
+     * @Security("has_role('ROLE_STAFF')")
+     */
+    public function showUsersAction(Request $request) {
         $users = $this->getDoctrine()
-                        ->getRepository('AppBundle:User')
-                        ->findAll();
-			
+                ->getRepository('AppBundle:User')
+                ->findAll();
+
         return $this->render('default/listusers.html.twig', array(
-				'users' => $users
+                    'users' => $users
         ));
     }
-	
-	/** Zeigt einen Benutzer mit Editierfunktion für Mitarbeiter
+
+    /** Zeigt einen Benutzer mit Editierfunktion für Mitarbeiter
      * @Route("/show/user/{id}", name="showuser")
      * @Security("has_role('ROLE_STAFF')")
      */
-    public function showUserAction(Request $request, $id)
-    {
+    public function showUserAction(Request $request, $id) {
         $user = $this->getDoctrine()
-                    ->getRepository('AppBundle:User')
-                    ->find($id);
+                ->getRepository('AppBundle:User')
+                ->find($id);
         $form = $this->createForm("AppBundle\Form\ProfilType", $user);
-		$form->handleRequest($request);
-        
-        if ($form->isSubmitted() && $form->isValid())
-		{
-			if ($request->request->has('delete'))
-            {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($request->request->has('delete')) {
                 return $this->redirectToRoute('removeuser', array(
-						'id' => $id,
-						'message' => "Nutzer wurde erfolgreich gelöscht!",
+                            'id' => $id,
+                            'message' => "Nutzer wurde erfolgreich gelöscht!",
                 ));
             }
-			
-			$em = $this->getDoctrine()->getManager();
+
+            $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
-            
+
             return $this->redirectToRoute('showuser', array(
-					'id' => $id,
-					'message' => "Daten wurden erfolgreich gespeichert!",
+                        'id' => $id,
+                        'message' => "Daten wurden erfolgreich gespeichert!",
             ));
         }
-        
+
         return $this->render('default/showuser.html.twig', array(
-				'user' => $user,
-				'form' => $form->createView(),
-				'message' => $request->query->get('message') 
+                    'user' => $user,
+                    'form' => $form->createView(),
+                    'message' => $request->query->get('message')
         ));
-	}
-	
-	/** Seinen eigenen Benutzeraccount löschen
+    }
+
+    /** Seinen eigenen Benutzeraccount löschen
      * @Route("/delete/user", name="deleteuser")
      * @Security("has_role('ROLE_USER')")
      */
-    public function deleteUserAction(Request $request)
-    {
+    public function deleteUserAction(Request $request) {
         $uid = $this->get('security.token_storage')->getToken()->getUser()->getId();
         $user = $this->getDoctrine()
-                           ->getRepository('AppBundle:User')
-                           ->find($uid);
+                ->getRepository('AppBundle:User')
+                ->find($uid);
+        $relationships = $this->getDoctrine()
+                ->getRepository('AppBundle:Kontakt')
+                ->findByUser($user);
+        $praktika = $this->getDoctrine()
+                ->getRepository('AppBundle:Praktikum')
+                ->findByUser($user);
         $em = $this->getDoctrine()->getManager();
+        foreach($relationships as $relationship){
+            $em->remove($relationship);
+        }
+        foreach ($praktika AS $praktikum) {
+            $em->remove($praktikum);
+        }
         $em->remove($user);
         $em->flush();
+        
+        //clear out session before redirecting deleted user
+        $this->get('security.context')->setToken(null);
+        $this->get('request')->getSession()->invalidate();
         
         return $this->redirectToRoute('logout');
     }
-	
-	/** Benutzer aus der Datenbank löschen
+
+    /** Benutzer aus der Datenbank löschen
      * @Route("/delete/user/{id}", name="removeuser")
      * @Security("has_role('ROLE_ADMIN')")
      */
-    public function removeUserAction(Request $request, $id)
-    {
+    public function removeUserAction(Request $request, $id) {
         $user = $this->getDoctrine()
-                        ->getRepository('AppBundle:User')
-                        ->find($id);
-		$praktika = $this->getDoctrine()
-                        ->getRepository('AppBundle:Praktikum')
-						->findByUser($user);
-		$kontakte = $this->getDoctrine()
-                        ->getRepository('AppBundle:Kontakt')
-						->findByUser($user);
+                ->getRepository('AppBundle:User')
+                ->find($id);
+        $praktika = $this->getDoctrine()
+                ->getRepository('AppBundle:Praktikum')
+                ->findByUser($user);
+        $kontakte = $this->getDoctrine()
+                ->getRepository('AppBundle:Kontakt')
+                ->findByUser($user);
         $em = $this->getDoctrine()->getManager();
-		foreach($praktika AS $praktikum)
-		{
-			$em->remove($praktikum);
-		}
-		foreach($kontakte AS $kontakt)
-		{
-			$em->remove($kontakt);
-		}
+        foreach ($praktika AS $praktikum) {
+            $em->remove($praktikum);
+        }
+        foreach ($kontakte AS $kontakt) {
+            $em->remove($kontakt);
+        }
         $em->remove($user);
         $em->flush();
         
+        //clear out session before redirecting deleted user
+        $this->get('security.context')->setToken(null);
+        $this->get('request')->getSession()->invalidate();
+        
         return $this->redirectToRoute('showusers');
     }
+
 }
